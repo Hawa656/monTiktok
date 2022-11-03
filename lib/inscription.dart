236fadde9b0cf79c468aeba5f583fcedd556b453
Hawa656 <107039828+Hawa656@users.noreply.github.com>
2022-11-03 19:54:08 +0000

    page inscription non terminé

---
+++ lib/inscription.dart
@@ -0,0 +1,293 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(Inscription());
+}
+
+class Inscription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('profile'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.person_outline,
+                color: Colors.grey,
+                size: 100.0,
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(
+                'Créer un nouveau compte',
+                style: TextStyle(color: Colors.black87),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              SizedBox(
+                width: 200.0,
+                child: Builder(
+                  builder: (context) => ElevatedButton(
+                      onPressed: () => {_openPopup(context)},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                        Colors.red[400],
+                      )),
+                      //textColor: Colors.white,
+                      child: Text(
+                        'S\'inscrire',
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void _openPopup(context) {
+  showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.white,
+      builder: (BuildContext bc) {
+        return Container(
+          height: MediaQuery.of(context).size.height * .90,
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: Icon(
+                          Icons.close,
+                        ),
+                      ),
+                      Icon(
+                        Icons.help,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Text(
+                    'Inscription à TikTok',
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    'Créer un profil, abonne-toi à d’autres comptes,\n crée tes propres vidéos et bien plus encore.',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.black54,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  ButtonTheme(
+                    minWidth: 250.0,
+                    child: ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.grey,
+                              width: 0.5,
+                            ),
+                          ),
+                          primary: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: FaIcon(
+                                  FontAwesomeIcons.user,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Utilise un téléphone ou une adresse  e-mail',
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+
+                  //************************premiere ligne********************************
+                  ButtonTheme(
+                    minWidth: 250.0,
+                    child: ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.grey,
+                              width: 0.5,
+                            ),
+                          ),
+                          primary: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: FaIcon(
+                                  FontAwesomeIcons.facebook,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Continuer avec Facebook',
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  //********************************** */
+                  ButtonTheme(
+                    minWidth: 250.0,
+                    child: ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.grey,
+                              width: 0.5,
+                            ),
+                          ),
+                          primary: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: FaIcon(
+                                  FontAwesomeIcons.google,
+                                  color: Colors.red,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Continuer avec Google',
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  //************* */
+                  ButtonTheme(
+                    minWidth: 250.0,
+                    child: ElevatedButton(
+                        onPressed: () => {},
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: Colors.grey,
+                              width: 0.5,
+                            ),
+                          ),
+                          primary: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: FaIcon(
+                                  FontAwesomeIcons.twitter,
+                                  color: Colors.lightBlue,
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Continuer avec Twitter ',
+                                  style: TextStyle(
+                                    fontSize: 15.0,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+import 'inscription.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -55,7 +57,7 @@ class _BottomNavbarState extends State<BottomNavbar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomePage(),
+      body: Inscription(),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
